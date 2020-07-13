fb316a3221cba799ae11f71b066b094129f38385
Lucas Becker <lucasgabrielbec@gmail.com>
2020-07-13 11:37:50 -0300

    Update app.js

--- app.js
+++ app.js
@@ -68,4 +68,4 @@ app.use('/tickets', require('./routes/tickets'));
 
 
 
-app.listen(9000, console.log(`Server running in mode ${process.env.NODE_ENV} at port ${port}`))
+app.listen(process.env.NODE_ENV, console.log(`Server running in mode ${process.env.NODE_ENV} at port ${port}`))
